8bca94e14dece0e79f42da91b90ddb4b2d93d543
Sen-1827 <powderrush9@gmail.com>
2024-08-26 21:37:02 +0530

    feat(pages): add a background
    Added a background

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,15 @@
 'use client';
 
 import NextImage from 'next/image';
-import { Image, Center, Flex, Text, Space } from '@mantine/core';
+import {
+  Image,
+  Center,
+  Flex,
+  Text,
+  Space,
+  Box,
+  BackgroundImage,
+} from '@mantine/core';
 import CustomAppShell from '@/components/custom_app_shell/custom_app_shell';
 import SparkLogoSrc from '@/public/images/spark_logo_transparent.png';
 import ButtonCard from '@/components/button_card/button_card';
@@ -12,95 +20,98 @@ function SparkHomePage() {
 
   return (
     <CustomAppShell>
-      <Flex
-        direction={'column'}
-        justify={'center'}
-        align={'center'}
-        gap={'xl'}
+      <BackgroundImage
+        h={400}
+        src={'/images/everest.png'}
       >
-        <Image
-          w={isMobile ? '100%' : '36%'}
-          fit={'contain'}
-          component={NextImage}
-          src={SparkLogoSrc}
-          alt={'Spark Logo'}
-          priority
-        />
-        {/* TODO - Match with exact Theme */}
-        <Text
-          fz={'h3'}
-          fs={'italic'}
-          c={'#A61FD6'}
-        >
-          Making the Impossible Possible
-        </Text>
-        <Center maw={isMobile ? '100%' : '60%'}>
-          {/* TODO - Make Text Center Aligned */}
-          <Text
-            ta={'center'}
-            fz={'h4'}
-          >
-            SPARK will expose ENTC undergraduates to a set of challenges not
-            made available in any other university in Asia, Europe, or the
-            Americas. This unique project will not only provide a platform to
-            develop and nurture innovative ideas to solve real-world problems,
-            but also develop some of the soft and hard skill sets required by
-            undergraduates to be effective leaders and consummate professionals
-            in the modern-day industry.”
-          </Text>
-        </Center>
-        <Space h={isMobile ? 'sm' : 'xl'} />
         <Flex
-          direction={isMobile ? 'column' : 'row'}
-          align={'center'}
+          direction={'column'}
           justify={'center'}
-          gap={isMobile ? 'md' : 'xl'}
+          align={'center'}
+          gap={'xl'}
         >
-          <ButtonCard
-            heading={'Spark Challenge'}
-            description={
-              'A Challenge for students to produce unique, novel products and services that mitigate the detrimental impact of Climate Change. '
-            }
-            imageSrc={'/images/everest.png'}
-            width={300}
-            height={450}
-            bgColor={
-              'linear-gradient(to bottom right, #343434 0%, #545454 100%)'
-            }
-            textColor={'white'}
-            href={'/challenge'}
+          <Image
+            w={isMobile ? '100%' : '36%'}
+            fit={'contain'}
+            component={NextImage}
+            src={SparkLogoSrc}
+            alt={'Spark Logo'}
+            priority
           />
+          {/* TODO - Match with exact Theme */}
+          <Text
+            fz={'h3'}
+            fs={'italic'}
+            c={'#A61FD6'}
+          >
+            Making the Impossible Possible
+          </Text>
+          <Center maw={isMobile ? '100%' : '60%'}>
+            {/* TODO - Make Text Center Aligned */}
+            <Text
+              ta={'center'}
+              fz={'h4'}
+            >
+              SPARK will expose ENTC undergraduates to a set of challenges not
+              made available in any other university in Asia, Europe, or the
+              Americas. This unique project will not only provide a platform to
+              develop and nurture innovative ideas to solve real-world problems,
+              but also develop some of the soft and hard skill sets required by
+              undergraduates to be effective leaders and consummate
+              professionals in the modern-day industry.”
+            </Text>
+          </Center>
+          <Space h={isMobile ? 'sm' : 'xl'} />
+          <Flex
+            direction={isMobile ? 'column' : 'row'}
+            align={'center'}
+            justify={'center'}
+            gap={isMobile ? 'md' : 'xl'}
+          >
+            <ButtonCard
+              heading={'Spark Challenge'}
+              description={
+                'A Challenge for students to produce unique, novel products and services that mitigate the detrimental impact of Climate Change. '
+              }
+              imageSrc={'/images/everest.png'}
+              width={300}
+              height={400}
+              bgColor={'purple'}
+              textColor={'white'}
+              href={'/challenge'}
+            />
 
-          <ButtonCard
-            heading={'Pi-Community'}
-            description={
-              'To build a Community; by exploiting the extensive Raspberry Pi resources available at ENTC.'
-            }
-            imageSrc={'/images/image_pi.png'}
-            width={300}
-            height={450}
-            bgColor={
-              'linear-gradient(to bottom right, #343434 0%, #545454 100%)'
-            }
-            textColor={'white'}
-            href={'/pi_community'}
-          />
-          <ButtonCard
-            heading={'UnderGraduate Development'}
-            description={
-              'Being a participant of this program undergraduates will develop important “life skills” required in the modern-day workplace.'
-            }
-            imageSrc={'/images/image_1.png'}
-            width={300}
-            height={450}
-            bgColor={
-              'linear-gradient(to bottom right, #343434 0%, #545454 100%)'
-            }
-            textColor={'white'}
-            href={'/undergraduate'}
-          />
+            <ButtonCard
+              heading={'Pi-Community'}
+              description={
+                'To build a Community; by exploiting the extensive Raspberry Pi resources available at ENTC.'
+              }
+              imageSrc={'/images/image_pi1.png'}
+              width={300}
+              height={400}
+              bgColor={'purple'}
+              textColor={'white'}
+              href={'/pi_community'}
+            />
+            <ButtonCard
+              heading={'UnderGraduate Development'}
+              description={
+                'Being a participant of this program undergraduates will develop important “life skills” required in the modern-day workplace.'
+              }
+              imageSrc={'/images/image_1.png'}
+              width={300}
+              height={400}
+              bgColor={'purple'}
+              textColor={'white'}
+              href={'/undergraduate'}
+            />
+          </Flex>
         </Flex>
-      </Flex>
+      </BackgroundImage>
+      <BackgroundImage
+        h={800}
+        src={'/images/image_1.png'}
+      ></BackgroundImage>
     </CustomAppShell>
   );
 }
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,7 +2,7 @@
 
 import CustomAppShell from '@/components/custom_app_shell/custom_app_shell';
 import SkeletonCard from '@/components/skeleton_card/skeleton_card';
-import { Button, Flex, Text } from '@mantine/core';
+import { BackgroundImage, Button, Flex, Text } from '@mantine/core';
 import Link from 'next/link';
 
 import CustomImage from '@/components/custom_image/custom_image';
@@ -10,73 +10,84 @@ import CustomImage from '@/components/custom_image/custom_image';
 function TestPage() {
   return (
     <CustomAppShell>
-      <Flex
-        direction={'column'}
-        justify={'flex-start'}
-        align={'flex-start'}
-        gap={'sm'}
+      <BackgroundImage
+        component='image'
+        color='red'
+        h={900}
+        src={'/images/image_1.png'}
       >
-        <Button
-          component={Link}
-          href='/'
-          radius={'md'}
-          size={'md'}
+        <Flex
+          direction={'column'}
+          justify={'flex-start'}
+          align={'flex-start'}
+          gap={'sm'}
         >
-          Home Page
-        </Button>
-        <Text>This is Test Page</Text>
-        <SkeletonCard text={'This is a test card'} />
-        <SkeletonCard
-          height={200}
-          width={300}
-          bgColor={'orange'}
-          textColor={'white'}
-          text={'This is a test card'}
-        />
-        <SkeletonCard
-          height={300}
-          width={600}
-          textSize={'36'}
-          textWeight={200}
-          bgColor={'red'}
-          textColor={'black'}
-          text={'This is also another test card'}
-        />
-        <SkeletonCard
-          height={200}
-          width={300}
-          bgColor={'orange'}
-          textColor={'white'}
-          text={'This is a test card'}
-        />
-        <SkeletonCard
-          height={200}
-          width={300}
-          bgColor={'orange'}
-          textColor={'white'}
-          text={'This is a test card'}
-        />
-        <SkeletonCard
-          height={200}
-          width={300}
-          bgColor={'orange'}
-          textColor={'white'}
-          text={'This is a test card'}
-        />
-        <SkeletonCard
-          height={200}
-          width={300}
-          bgColor={'orange'}
-          textColor={'white'}
-          text={'This is a test card'}
-        />
-        <CustomImage
-          width={300}
-          height={400}
-          radius='xl'
-          imageSrc={'/images/image_1.png'}
-        />
-      </Flex>
+          <Button
+            component={Link}
+            href='/'
+            radius={'md'}
+            size={'md'}
+          >
+            Home Page
+          </Button>
+          <Text>This is Test Page</Text>
+          <SkeletonCard text={'This is a test card'} />
+          <SkeletonCard
+            height={200}
+            width={300}
+            bgColor={'orange'}
+            textColor={'white'}
+            text={'This is a test card'}
+          />
+          <SkeletonCard
+            height={300}
+            width={600}
+            textSize={'36'}
+            textWeight={200}
+            bgColor={'red'}
+            textColor={'black'}
+            text={'This is also another test card'}
+          />
+          <SkeletonCard
+            height={200}
+            width={300}
+            bgColor={'orange'}
+            textColor={'white'}
+            text={'This is a test card'}
+          />
+          <SkeletonCard
+            height={200}
+            width={300}
+            bgColor={'orange'}
+            textColor={'white'}
+            text={'This is a test card'}
+          />
+          <SkeletonCard
+            height={200}
+            width={300}
+            bgColor={'orange'}
+            textColor={'white'}
+            text={'This is a test card'}
+          />
+          <SkeletonCard
+            height={200}
+            width={300}
+            bgColor={'orange'}
+            textColor={'white'}
+            text={'This is a test card'}
+          />
+          <CustomImage
+            width={300}
+            height={400}
+            radius='xl'
+            imageSrc={'/images/image_1.png'}
+          />
+        </Flex>
+      </BackgroundImage>
+      <BackgroundImage
+        h={800}
+        src={'/images/everest.png'}
+      ></BackgroundImage>
     </CustomAppShell>
   );
 }
