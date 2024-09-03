7b5273da7caaa71f06e624f7e35bad022da99cf7
Yadola <Ya-dola@users.noreply.github.com>
2024-09-03 22:12:25 +0530

    refactor(json-utils): add named parameters
    Added named parameters

--- app/pi_community/page.tsx
+++ app/pi_community/page.tsx
@@ -1,11 +1,15 @@
-import { PiMoraModel } from '@/models/pi_mora/pi_mora_model';
-import PiCommunityClientPage from '@/pages/pi_community_page/pi_community_page';
+import { CommunitySectionModel } from '@/models/community/community_section_model';
+import PiCommunityClient from '@/clients/pi_community/pi_community_client';
 import { getJsonData } from '@/utils/json_utils';
 
-async function CommunityPage() {
-  const piMoraModel: PiMoraModel = await getJsonData<PiMoraModel>('pi_mora');
+async function PiCommunityPage() {
+  const piMoraSection: CommunitySectionModel =
+    await getJsonData<CommunitySectionModel>({
+      page: 'community',
+      fileName: 'pi_mora',
+    });
 
-  return <PiCommunityClientPage piMoraData={piMoraModel} />;
+  return <PiCommunityClient piMoraData={piMoraSection} />;
 }
 
-export default CommunityPage;
+export default PiCommunityPage;
--- utils/json_utils.ts
+++ utils/json_utils.ts
@@ -1,11 +1,21 @@
 import fs from 'fs';
 import path from 'path';
 
-export async function getJsonData<T>(
-  fileName: string,
-  directory: string = 'constants',
-): Promise<T> {
-  const filePath = path.join(process.cwd(), directory, `${fileName}.json`);
+export async function getJsonData<T>({
+  directory = 'constants',
+  page,
+  fileName,
+}: {
+  directory?: string;
+  page: string;
+  fileName: string;
+}): Promise<T> {
+  const filePath = path.join(
+    process.cwd(),
+    directory,
+    page,
+    `${fileName}.json`,
+  );
   const fileContents = await fs.promises.readFile(filePath, 'utf8');
   return JSON.parse(fileContents);
 }
