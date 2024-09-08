e4e88af5fa0960ce33b5c671827aa235ccbb63aa
Sen-1827 <powderrush9@gmail.com>
2024-09-08 14:52:03 +0530

    feat(button-card): make button card pop out
    Make button card pop out when you hover over them

--- components/button_card/button_card.tsx
+++ components/button_card/button_card.tsx
@@ -1,6 +1,8 @@
-import { Image, Text, Card, MantineSize } from '@mantine/core';
+import { Image, Text, Card, MantineSize, Button, Space } from '@mantine/core';
 import Link from 'next/link';
+import React, { useState } from 'react';
 import NextImage from 'next/image';
+import { useIsMobile } from '@/utils/breakpoint_utils';
 
 interface ButtonCardProps {
   heading: string;
@@ -12,9 +14,11 @@ interface ButtonCardProps {
   radius?: string;
   bgColor?: string;
   textColor?: string;
-  headingSize?: MantineSize;
+  headingColor?: string;
+  headingSize?: string;
   headingWeight?: number;
   textSize?: MantineSize;
+  buttonColor?: string;
 }
 
 function ButtonCard({
@@ -27,21 +31,33 @@ function ButtonCard({
   radius = 'md',
   bgColor = 'purple',
   textColor = 'white',
-  headingSize = 'lg',
-  headingWeight = 700,
+  headingColor = 'white',
+  headingSize = 'h3',
+  headingWeight = 900,
   textSize = 'sm',
+  buttonColor = '#262626',
 }: ButtonCardProps) {
   const cardHeight = height ?? 200 * 2;
+  const [isHovered, setIsHovered] = useState(false);
+  const isMobile = useIsMobile();
 
   return (
     <Card
       h={cardHeight}
       w={width}
       bg={bgColor}
-      padding={'xl'}
+      padding={'sm'}
       component={Link}
       href={href}
       radius={radius}
+      withBorder
+      style={{
+        transform: isHovered && !isMobile ? 'scale(1.1) ' : 'scale(1)', // Scale card on hover
+        transition: 'transform 0.3s ease',
+        zIndex: isHovered && !isMobile ? 1 : 'auto', // Ensure hovered card is on top
+      }}
+      onMouseEnter={() => !isMobile && setIsHovered(true)}
+      onMouseLeave={() => !isMobile && setIsHovered(false)}
     >
       <Card.Section>
         <Image
@@ -55,28 +71,38 @@ function ButtonCard({
       </Card.Section>
 
       <Text
-        c={textColor}
+        c={headingColor}
         fw={headingWeight}
-        size={headingSize}
+        fz={headingSize}
+        px={'md'}
         mt={'md'}
       >
         {heading}
       </Text>
       <Text
         c={textColor}
+        fw={700}
         size={textSize}
         mt={'md'}
+        px={'md'}
       >
         {description}
       </Text>
 
-      <Text
-        td={'underline'}
+      <Button
+        color={buttonColor}
+        radius={'xl'}
         mt={'auto'}
         ml={'auto'}
       >
-        Learn More
-      </Text>
+        <Text
+          /*td={'underline'}*/ fw={900}
+          fz={'sm'}
+          c={'#262626'}
+        >
+          Learn More
+        </Text>
+      </Button>
     </Card>
   );
 }
