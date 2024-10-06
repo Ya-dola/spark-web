7fcbd54cf889859c3dc511cd78a60a8824e189dc
Sen-1827 <powderrush9@gmail.com>
2024-10-06 19:08:10 +0530

    fix(page): fix go to home
    Fixed go to home

--- app/home/page.tsx
+++
@@ -1,7 +0,0 @@
-import SparkHomeClient from '@/clients/spark_home/spark_home_client';
-
-function SparkHomePage() {
-  return <SparkHomeClient />;
-}
-
-export default SparkHomePage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,7 @@
-import { Button, Flex } from '@mantine/core';
-import Link from 'next/link';
+import SparkHomeClient from '@/clients/spark_home/spark_home_client';
 
-function HomePage() {
-  return (
-    <Flex
-      direction={'column'}
-      justify={'flex-start'}
-      align={'flex-start'}
-      gap={'xl'}
-      p={'lg'}
-    >
-      <Button
-        fullWidth
-        h={'25vh'}
-        color={'grape'}
-        component={Link}
-        size={'md'}
-        radius={'md'}
-        href={'/home'}
-      >
-        Spark Web App
-      </Button>
-
-      <Button
-        fullWidth
-        h={'15vh'}
-        color={'blue'}
-        component={Link}
-        size={'md'}
-        radius={'md'}
-        href={'/test'}
-      >
-        Test Page
-      </Button>
-    </Flex>
-  );
+function SparkHomePage() {
+  return <SparkHomeClient />;
 }
 
-export default HomePage;
+export default SparkHomePage;
