bb13a44333904045e04434aa2891307fb8cec07e
Yadola <Ya-dola@users.noreply.github.com>
2024-10-12 23:43:51 +0530

    refactor(pi-community): add comments
    Added comments

--- app/pi_community/page.tsx
+++ app/pi_community/page.tsx
@@ -3,18 +3,21 @@ import PiCommunityClient from '@/clients/pi_community/pi_community_client';
 import { getJsonData } from '@/utils/json_utils';
 
 async function PiCommunityPage() {
+  // Fetch the PI-MORA section data
   const piMoraSection: CommunitySectionModel =
     await getJsonData<CommunitySectionModel>({
       page: 'community',
       fileName: 'pi_mora',
     });
 
+  // Fetch the Tech Demo section data
   const techDemoSection: CommunitySectionModel =
     await getJsonData<CommunitySectionModel>({
       page: 'community',
       fileName: 'tech_demo',
     });
 
+  // Fetch the Final Year section data
   const finalYearSection: CommunitySectionModel =
     await getJsonData<CommunitySectionModel>({
       page: 'community',
