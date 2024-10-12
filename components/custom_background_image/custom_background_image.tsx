2c0bc7f7803057d63dd7005a506ee06a49b3c4e2
Yadola <Ya-dola@users.noreply.github.com>
2024-10-12 23:34:16 +0530

    refactor(custom-background-image): add comments
    Added comments

--- components/custom_background_image/custom_background_image.tsx
+++ components/custom_background_image/custom_background_image.tsx
@@ -8,46 +8,57 @@ interface CustomBackgroundImageProps {
   pos?: string;
 }
 
-const CustomBackgroundImage: React.FC<CustomBackgroundImageProps> = ({
+function CustomBackgroundImage({
   height,
   imageSrc,
   bgSize = 'cover',
   pos = 'top center',
-}) => {
+}: CustomBackgroundImageProps) {
   return (
-    <div //container for background
+    <div
+      // Container for the background image
       style={{
-        position: 'absolute', // Position is absolute to cover the entire area of the component
+        // Ensure the container covers the entire area
+        position: 'absolute',
         top: 0,
         left: 0,
         right: 0,
         bottom: 0,
-        height: height, // Set the height for each BackgroundImage
+        // Set the height of the background container
+        height: height,
       }}
     >
       <BackgroundImage
-        src={imageSrc} //url
+        // URL for the background image
+        src={imageSrc}
         style={{
-          height: '100%', //Fill the container's height
-          backgroundSize: bgSize, // Crop the image (cover, contain, etc)
-          backgroundPosition: pos, // Anchor position of the image
+          // Fill the entire height of the container
+          height: '100%',
+          // Set how the image should scale (cover, contain, etc.)
+          backgroundSize: bgSize,
+          // Position the background image (e.g., top, center)
+          backgroundPosition: pos,
         }}
       />
-      <div //overlay graident to blend in the bg
+      <div
+        // Overlay with a gradient effect
         style={{
+          // Covers the entire container area
           position: 'absolute',
           top: 0,
           left: 0,
           right: 0,
           bottom: 0,
+          // Transparent at the top, fading to black at the bottom
           background:
             'linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 1) 100%)',
+          // Ensures the gradient overlay doesn't block
+          // interactions with the background image
           pointerEvents: 'none',
-          // Ensures the gradient overlay doesn't prevent interactions with the image
         }}
       />
     </div>
   );
-};
+}
 
 export default CustomBackgroundImage;
